f6ff775dfe393f7a6e62668175c5212026e9c1eb
Sushi Chanrai <sushi@pathworks.co>
2018-07-08 08:46:39 +0000

    Done.

---
+++ test.rb
@@ -0,0 +1,24 @@
+holiday_hash = {
+   :winter => {
+     :christmas => ["Lights", "Wreath"],
+     :new_years => ["Party Hats"]
+  },
+   :summer => {
+     :fourth_of_july => ["Fireworks", "BBQ"]
+   },
+   :fall => {
+     :thanksgiving => ["Turkey"]
+   },
+   :spring => {
+     :memorial_day => ["BBQ"]
+   }
+ }
+
+
+ # iterate through holiday_hash and print items such that your readout resembles:
+ # Winter:
+ #   Christmas: Lights, Wreath
+ #   New Years: Party Hats
+ # Summer:
+ #   Fourth Of July: Fireworks, BBQ
+ # etc.
